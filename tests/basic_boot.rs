d6e830d2b9d90a2e5db6f4fec2cfff51dae7c031
Vinicius de Borba <vinicius.borba@hotmail.com>
2024-10-11 00:03:15 -0300

    post03 + post04 - complete VGA text module + IO serial ports + testing

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,27 +5,22 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+
 use rust_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rust_os::test_panic_handler(info);
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
