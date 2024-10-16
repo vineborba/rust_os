a332756c31774f0af23bfef9e71ae54815ceee4b
Vinicius de Borba <vinicius.borba@hotmail.com>
2024-10-15 22:37:09 -0300

    post05 - introduction to interruptions

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
 
+pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
 
@@ -40,9 +42,14 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     loop {}
 }
 
+pub fn init() {
+    interrupts::init_idt();
+}
+
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
 }
