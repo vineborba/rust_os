a19fdd77c167f1f0a604ff3b007f6af56ecfd1e6
Vinicius de Borba <vinicius.borba@hotmail.com>
2024-10-30 00:28:15 -0300

    post07 - hardware interrupts

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -142,7 +142,11 @@ macro_rules! println {
 #[doc(hidden)]
 pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
-    WRITER.lock().write_fmt(args).unwrap();
+    use x86_64::instructions::interrupts;
+
+    interrupts::without_interrupts(|| {
+        WRITER.lock().write_fmt(args).unwrap();
+    });
 }
 
 #[cfg(test)]
@@ -163,11 +167,17 @@ mod tests {
 
     #[test_case]
     fn test_println_output() {
+        use core::fmt::Write;
+        use x86_64::instructions::interrupts;
+
         let s = "Some random string that fits";
-        println!("{}", s);
-        for (i, c) in s.chars().enumerate() {
-            let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
-            assert_eq!(char::from(screen_char.ascii_character), c);
-        }
+        interrupts::without_interrupts(|| {
+            let mut writer = WRITER.lock();
+            writeln!(writer, "\n{}", s).expect("writln failed");
+            for (i, c) in s.chars().enumerate() {
+                let screen_char = writer.buffer.chars[BUFFER_HEIGHT - 2][i].read();
+                assert_eq!(char::from(screen_char.ascii_character), c);
+            }
+        });
     }
 }
